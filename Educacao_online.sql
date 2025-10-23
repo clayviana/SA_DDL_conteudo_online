e042ca716612c3da2526d3a62d8879c7f8c0d544
clayviana <clayviana24@gmail.com>
2025-10-23 20:56:34 -0300

    Add files via upload
    Entrega inicial dos sripts DDL

---
+++ Educacao_online.sql
@@ -0,0 +1,50 @@
+create database Educacao_online;
+USE Educacao_online;
+CREATE TABLE usuarios(
+usuario_id INT PRIMARY KEY,
+nome VARCHAR(150) NOT NULL,
+email VARCHAR(150) NOT NULL UNIQUE,
+data_cadastro DATE NOT NULL
+);
+
+CREATE TABLE professores(
+id_professor INT PRIMARY KEY,
+nome VARCHAR(150) NOT NULL,
+especialidade  VARCHAR(100) NOT NULL,
+salario_base DECIMAL(10,2)
+);
+CREATE TABLE cursos(
+cursos_id INT PRIMARY KEY,
+titulo VARCHAR(255) NOT NULL UNIQUE,
+descriçao TEXT,
+carga_horaria INT,
+data_lancamento DATE
+);
+CREATE TABLE aulas( 
+aula_id INT PRIMARY KEY,
+titulo_aula VARCHAR(255) NOT NULL,
+duracao_minutos INT NOT NULL,
+tipo_conteudo VARCHAR(50)
+);
+ALTER TABLE professores 
+ADD COLUMN email_corporativo VARCHAR(150);
+
+ALTER TABLE cursos
+MODIFY COLUMN carga_horaria DECIMAL (5,2);
+DROP TABLE aulas;
+CREATE TABLE aulas( 
+aula_id INT PRIMARY KEY,
+titulo_aula VARCHAR(255) NOT NULL,
+duracao_minutos INT NOT NULL,
+tipo_conteudo VARCHAR(50)
+);
+
+
+
+
+
+
+
+
+
+
